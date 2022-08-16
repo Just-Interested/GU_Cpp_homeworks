c1bd8a040962fdc14fece3f027bda39830940f1c
Stiven Diskein <sdiskein@ya.ru>
2022-08-16 16:54:56 +0000

    add parsers

--- Cpp_difficult_moments/hw_1/main.cpp
+++ Cpp_difficult_moments/hw_1/main.cpp
@@ -47,14 +47,30 @@ std::ostream &operator<<(std::ostream &os, PhoneNumber &ph_num)
     return os;
 }
 
-bool operator<(PhoneNumber &ph_num1, PhoneNumber &ph_num2)
+bool operator<(PhoneNumber ph_num1, PhoneNumber ph_num2)
 {
     return std::tie(ph_num1.country_code, ph_num1.city_code, ph_num1.number, ph_num1.additional_number) < std::tie(ph_num2.country_code, ph_num2.city_code, ph_num2.number, ph_num2.additional_number);
 }
 
-bool operator==(PhoneNumber &ph_num1, PhoneNumber &ph_num2)
+bool operator==(PhoneNumber ph_num1, PhoneNumber ph_num2)
 {
     return std::tie(ph_num1.country_code, ph_num1.city_code, ph_num1.number, ph_num1.additional_number) == std::tie(ph_num2.country_code, ph_num2.city_code, ph_num2.number, ph_num2.additional_number);
+}
+
+std::vector<std::string> split_string(std::string src, char delimiter){
+    std::vector<std::string> v;
+    int src_len = src.length();
+    int last_pos = 0;
+    for (int i = 0; i < src_len; i++){
+        if (src.at(i) == delimiter){
+            if (last_pos != i)      // исключаем подряд идущие разделители
+                v.push_back(src.substr(last_pos, i - last_pos));
+            last_pos = i + 1;
+        }
+    }
+    if (last_pos < src_len)
+        v.push_back(src.substr(last_pos, src_len - last_pos));
+    return v;
 }
 
 
@@ -79,81 +95,66 @@ public:
 
 };
 
+
 PhoneNumber parse_number(std::string s){
     PhoneNumber ph_num;
-    int plus_pos = -1;
-    int open_brace_pos = -1;
-    int close_brace_pos = -1;
-    int space_pos = -1;
     for (int i = 0; i < s.length(); i++){
-        if (s[i] == '+')
-            plus_pos = i;
-        if (s[i] == '(')
-            open_brace_pos = i;
-        if (s[i] == ')')
-            close_brace_pos = i;
-        if (s[i] == ' ')
-            space_pos = i;
+        char t = s[i];
+        if (t == '+' || t == '(' || t == ')')
+            s[i] = ' ';
     }
-    ph_num.country_code = std::stoi(s.substr(plus_pos + 1, open_brace_pos));
-    ph_num.city_code = std::stoi(s.substr(open_brace_pos + 1, close_brace_pos));
-    if (space_pos != -1){
-        ph_num.number = std::stoi(s.substr(close_brace_pos + 1, space_pos));
-        ph_num.additional_number = std::stoi(s.substr(space_pos + 1));
-    }
-    ph_num.number = std::stoi(s.substr(close_brace_pos + 1));
+    std::vector<std::string> v = split_string(s, ' ');
+    if (v.size() > 4 || v.size() < 3)
+        return PhoneNumber();
+    if (v.size() == 4)
+        ph_num.additional_number = std::stoi(v.back()) ; v.pop_back();
+    ph_num.number = std::stoi(v.back()) ; v.pop_back();
+    ph_num.city_code = std::stoi(v.back()) ; v.pop_back();
+    ph_num.country_code = std::stoi(v.back()) ; v.pop_back();
     return ph_num;
+}
+
+Person parse_fullname(std::string s){
+    Person p;
+    std::vector<std::string> splited = split_string(s, ' ');
+    int size = splited.size();
+    if (size > 3 || size < 2)
+        return Person();
+    if (size == 3){
+        p.patronymic = splited.back(); splited.pop_back();
+    }
+    p.surname = splited.back(); splited.pop_back();
+    p.name = splited.back();
+    return p;
+}
+
+
+std::pair<Person, PhoneNumber> parse_record(std::string record){
+    Person p;
+    PhoneNumber ph_num;
+    int splitter = record.find('+');
+    p = parse_fullname(record.substr(0, splitter - 1));
+    ph_num = parse_number(record.substr(splitter, record.size() - splitter));
+    return std::pair<Person, PhoneNumber>(p, ph_num);
 }
 
 int main(int argc, char const *argv[])
 {
-    // std::cout << "Hello, ";
-    // Person p1{"Pavel", "Kucheryaviy", "Yurevich"};
-    // Person p2{"Андрей", "Неизвестный"};
-    // Person p3{"Андрей", "Известный"};
-    // Person p4{"Андрей", "Известный", "Андреевич"};
-    // Person p5{"Андрей", "Известный", "Яковлевич"};
-    // //std::cout << p.name << " " << p.surname << std::endl;
-    // std::cout << p1 << std::endl;
-    // std::cout << p2 << std::endl;
-    // std::cout << (p3 < p2) << std::endl;
-    // std::cout << (p3 < p4) << std::endl;
-    // std::cout << (p4 < p5) << std::endl;
-    // std::cout << (p3 < p1) << std::endl;
-    // std::cout << (p3 == p3) << std::endl;
-    // PhoneNumber ph_num{7, 987, 123456, 20};
-    // std::cout << ph_num << std::endl;
-    // std::ifstream file("G:\\phonebook.txt");
-    // PhoneBook book(file);
-    // std::stringstream stream("123 456");
-    // std::string word1;
-    // std::string word2;
-    // std::optional<std::string> word3;
-    // std::string word4;
-    // stream >> word1 >> word2 >> word4;
-    // std::cout << word1 << word2 << word4.empty();
-    std::string phone("+7(17)455767 57");
-    std::string phone2("+493(7637)6114861");
-    int pos = phone.rfind(" ");
-    std::cout << phone.substr(pos + 1) << std::endl;
-    std::optional<std::string> add_n;
-    add_n = phone.substr(pos + 1);
-    std::cout << *add_n << std::endl;
-    pos = phone2.rfind(" ");
-    std::cout << pos << std::endl;
-    std::cout << phone2.substr(pos + 1) << std::endl;
-
     std::string record("Ильин Петр Артемович +7(17)455767 57");
-    std::string p;
-    std::string num;
-    pos = record.find("+");
-    p = record.substr(0, pos);
-    num = record.substr(pos);
-    std::cout << p << " --- " << num << std::endl;
+    std::string src("123 456 789 aaa bbb ccc  444  555     ");
+    std::vector<std::string> v = split_string(record, '+');
+    for (auto str : v){
+        std::cout << str << std::endl;
+    }
 
-    PhoneNumber pn = parse_number("+7(17)455767 57");
-    std::cout << pn << std::endl;
-    pn = parse_number("+493(7637)6114861");
-    std::cout << pn << std::endl;
+    Person p = parse_fullname("Ильин Петр");
+    std::cout << (p == Person()) << p << std::endl;
+
+    PhoneNumber n = parse_number("7 ( 17 )455767 57");
+    std::cout << (n == PhoneNumber()) << n << std::endl;
+
+    std::pair<Person, PhoneNumber> pp = parse_record("Ильин Петр Артемович +7(17)455767 57");
+    std::cout << "Person: " << pp.first << std::endl;
+    std::cout << "Number: " << pp.second << std::endl;
     return 0;
 }
